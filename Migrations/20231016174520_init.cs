f9ee5be09af3657b8662fb98a92da6f55ff87107
Вова Якорев <vovanyakorev2@gmail.com>
2023-10-16 20:33:13 +0200

    Add gitignore

---
+++ Migrations/20231016174520_init.cs
@@ -0,0 +1,151 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SchoolHubApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Username = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PasswordHash = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    PasswordSalt = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    Pesel = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Username);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Parents",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserDataUsername = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Parents", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Parents_Users_UserDataUsername",
+                        column: x => x.UserDataUsername,
+                        principalTable: "Users",
+                        principalColumn: "Username",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pupils",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserDataUsername = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pupils", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Pupils_Users_UserDataUsername",
+                        column: x => x.UserDataUsername,
+                        principalTable: "Users",
+                        principalColumn: "Username",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Teachers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserDataUsername = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teachers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Teachers_Users_UserDataUsername",
+                        column: x => x.UserDataUsername,
+                        principalTable: "Users",
+                        principalColumn: "Username",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ParentPupil",
+                columns: table => new
+                {
+                    ChildrenId = table.Column<int>(type: "int", nullable: false),
+                    ParentsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ParentPupil", x => new { x.ChildrenId, x.ParentsId });
+                    table.ForeignKey(
+                        name: "FK_ParentPupil_Parents_ParentsId",
+                        column: x => x.ParentsId,
+                        principalTable: "Parents",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_ParentPupil_Pupils_ChildrenId",
+                        column: x => x.ChildrenId,
+                        principalTable: "Pupils",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ParentPupil_ParentsId",
+                table: "ParentPupil",
+                column: "ParentsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Parents_UserDataUsername",
+                table: "Parents",
+                column: "UserDataUsername");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pupils_UserDataUsername",
+                table: "Pupils",
+                column: "UserDataUsername");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Teachers_UserDataUsername",
+                table: "Teachers",
+                column: "UserDataUsername");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ParentPupil");
+
+            migrationBuilder.DropTable(
+                name: "Teachers");
+
+            migrationBuilder.DropTable(
+                name: "Parents");
+
+            migrationBuilder.DropTable(
+                name: "Pupils");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
